6066e1914bede825c23ce5a3082b57c984366fe0
Catalin Balan <laserbeam3@users.noreply.github.com>
2021-12-05 10:59:56 +0200

    Day 1

--- src/day01.zig
+++ src/day01.zig
@@ -9,10 +9,34 @@ const Str = []const u8;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("../data/day01.txt");
+const data = @embedFile("../data/puzzle/day01.txt");
 
 pub fn main() !void {
+    var countA: u32 = 0;
+    var countB: u32 = 0;
+    var prev: i32 = 9999999;
+    var ringBuffer = [3]i32{ 9999999, 0, 0 };
+    var bufferSum: i32 = 9999999;
+    var idx: u32 = 0;
 
+    var lines = tokenize(data, "\r\n");
+    while (lines.next()) |line| {
+        var x: i32 = parseInt(i32, line, 10) catch unreachable;
+        if (x > prev) {
+            countA += 1;
+        }
+        prev = x;
+
+        if (idx > 2 and bufferSum + x - ringBuffer[idx % 3] > bufferSum) {
+            countB += 1;
+        }
+        bufferSum -= ringBuffer[idx % 3];
+        ringBuffer[idx % 3] = x;
+        bufferSum += x;
+        idx += 1;
+    }
+    print("{}\n", .{countA});
+    print("{}\n", .{countB});
 }
 
 // Useful stdlib functions
