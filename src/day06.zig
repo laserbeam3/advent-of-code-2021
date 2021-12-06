abc53d6fd38336364555596fef9f285aaed9fec6
Catalin Balan <laserbeam3@users.noreply.github.com>
2021-12-06 08:15:54 +0200

    Learn about generating structs and the (at)This builtin.

--- src/day06.zig
+++ src/day06.zig
@@ -11,36 +11,72 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/puzzle/day06.txt");
 
-pub fn main() !void {
-    var counts = [_]u64{0} ** 9;
+fn FishWorld(comptime T: type, comptime fish_life: usize) type {
+    return struct {
+        day: u64 = 1,
+        counts: [fish_life]T = [_]T{0} ** fish_life,
+        const Self = @This();
 
+        fn advance(self: *Self) void {
+            self.day += 1;
+
+            var c0 = self.counts[0];
+            var idx: usize = 0;
+            while (idx < fish_life - 1) : (idx += 1) {
+                self.counts[idx] = self.counts[idx + 1];
+            }
+            self.counts[6] += c0;
+            self.counts[8] = c0;
+        }
+
+        fn totalFish(self: Self) T {
+            var total: T = 0;
+            for (self.counts) |count| {
+                total += count;
+            }
+            return total;
+        }
+    };
+}
+
+pub fn main() !void {
+    var counts: [9]u64 = [_]u64{0} ** 9;
+    var world = FishWorld(u64, 9){};
     var words = tokenize(data, ",\r\n");
     while (words.next()) |line| {
         const x = parseInt(u64, line, 10) catch unreachable;
         counts[x] += 1;
+        world.counts[x] += 1;
     }
 
     // Pass 1, let's simulate.
     // Fairly sure there's an O(1) solution as well...
     var day: u64 = 1;
-    var lastDay: u64 = 256;
-    while (day <= lastDay) : (day += 1) {
-
+    var last_day: u64 = 256;
+    while (day <= last_day) : (day += 1) {
         var c0 = counts[0];
         var idx: u64 = 0;
-        var totalFish: u64 = 0;
+        var total_fish: u64 = 0;
         while (idx < 8) : (idx += 1) {
             counts[idx] = counts[idx + 1];
-            totalFish += counts[idx];
+            total_fish += counts[idx];
         }
 
         counts[8] = c0;
         counts[6] += c0;
-        totalFish += 2 * c0;
+        total_fish += 2 * c0;
 
         // Ok, I kinda want to print a bit of history, but I don't want to show eeeverything...
-        if (day < 10 or day % 10 == 0 or lastDay / 10 == day / 10) {
-            print("{d: >3} {}\n", .{ day, totalFish });
+        if (day < 10 or day % 10 == 0 or last_day / 10 == day / 10) {
+            print("{d: >3} {}\n", .{ day, total_fish });
+        }
+    }
+
+    // V2. Using a struct;
+    while (world.day <= last_day) {
+        world.advance();
+        if (world.day < 10 or world.day % 10 == 0 or last_day / 10 == world.day / 10) {
+            print("{d: >3} {}\n", .{ world.day, world.totalFish() });
         }
     }
 }
