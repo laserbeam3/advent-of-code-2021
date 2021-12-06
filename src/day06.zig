34386e11aeca03bbb3e3786b01533cba42df8d0c
Catalin Balan <laserbeam3@users.noreply.github.com>
2021-12-06 07:41:10 +0200

    Day 6

--- src/day06.zig
+++ src/day06.zig
@@ -9,10 +9,42 @@ const Str = []const u8;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("../data/day06.txt");
+const data = @embedFile("../data/puzzle/day06.txt");
 
 pub fn main() !void {
-    
+    var counts = [_]u64{0} ** 9;
+
+    var words = tokenize(data, ",\r\n");
+    while (words.next()) |line| {
+        const x = parseInt(u64, line, 10) catch unreachable;
+        counts[x] += 1;
+    }
+
+    // Pass 1, let's simulate.
+    // Fairly sure there's an O(1) solution as well...
+    var day: u64 = 1;
+    var lastDay: u64 = 256;
+    while (day <= lastDay) {
+        defer day += 1;
+
+        var c0 = counts[0];
+        var idx: u64 = 0;
+        var totalFish: u64 = 0;
+        while (idx < 8) {
+            defer idx += 1;
+            counts[idx] = counts[idx + 1];
+            totalFish += counts[idx];
+        }
+
+        counts[8] = c0;
+        counts[6] += c0;
+        totalFish += 2 * c0;
+
+        // Ok, I kinda want to print a bit of history, but I don't want to show eeeverything...
+        if (day < 10 or day % 10 == 0 or lastDay / 10 == day / 10) {
+            print("{d: >3} {}\n", .{ day, totalFish });
+        }
+    }
 }
 
 // Useful stdlib functions
